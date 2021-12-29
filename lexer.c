275a7e17343184bebacda81159678b6ae4de65d0
Daniel McCarthy <daniel@dragonzap.com>
2021-12-29 15:24:39 +0000

    Creating a identifier token

--- lexer.c
+++ lexer.c
@@ -3,6 +3,7 @@
 #include "helpers/buffer.h"
 #include <string.h>
 #include <assert.h>
+#include <ctype.h>
 
 #define LEX_GETC_IF(buffer, c, exp)     \
     for (c = peekc(); exp; c = peekc()) \
@@ -285,6 +286,32 @@ static struct token* token_make_symbol()
     return token;
 }
 
+static struct token* token_make_identifier_or_keyword()
+{
+    struct buffer* buffer = buffer_create();
+    char c = 0;
+    LEX_GETC_IF(buffer, c, (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9') || c == '_');
+
+    // null terminator
+    buffer_write(buffer, 0x00);
+
+    // Check if this is a keyword
+
+    return token_create(&(struct token){.type=TOKEN_TYPE_IDENTIFIER,.sval=buffer_ptr(buffer)});
+}
+
+struct token* read_special_token()
+{
+    char c =peekc();
+    if (isalpha(c) || c == '_')
+    {
+        return token_make_identifier_or_keyword();
+    }
+
+    return NULL;
+}
+
+
 struct token *read_next_token()
 {
     struct token *token = NULL;
@@ -317,7 +344,11 @@ struct token *read_next_token()
         break;
 
     default:
-        compiler_error(lex_process->compiler, "Unexpected token\n");
+        token = read_special_token();
+        if (!token)
+        {
+            compiler_error(lex_process->compiler, "Unexpected token\n");
+        }
     }
     return token;
 }
