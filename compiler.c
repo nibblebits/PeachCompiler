fda5aca7e173836f7e02267ca3a56166b620f9dc
Daniel McCarthy <daniel@dragonzap.com>
2022-01-07 15:15:19 +0000

    Finalizing our lexer

--- compiler.c
+++ compiler.c
@@ -45,6 +45,8 @@ int compile_file(const char* filename, const char* out_filename, int flags)
         return COMPILER_FAILED_WITH_ERRORS;
     }
 
+    process->token_vec = lex_process->token_vec;
+
     // Preform parsing
 
     // Preform code generation..
--- compiler.h
+++ compiler.h
@@ -162,6 +162,9 @@ struct compile_process
         const char* abs_path;
     } cfile;
 
+
+    // A vector of tokens from lexical analysis.
+    struct vector* token_vec;
     FILE* ofile;
 
 };
