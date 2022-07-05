0efee0a5dd6b896ff064a55f5838f369bf5dd7bd
Daniel McCarthy <daniel@dragonzap.com>
2022-07-05 13:09:47 +0100

    Lecture 100 - Adjusting the function stack size

--- parser.c
+++ parser.c
@@ -1239,7 +1239,13 @@ void parse_body(size_t *variable_size, struct history *history)
     parse_body_multiple_statements(variable_size, body_vec, history);
     parser_scope_finish();
 
-#warning "Don't forget to adjust the function stack size"
+    if (variable_size)
+    {
+        if (history->flags & HISTORY_FLAG_INSIDE_FUNCTION_BODY)
+        {
+            parser_current_function->func.stack_size += *variable_size;
+        }
+    }
 }
 
 void parse_struct_no_new_scope(struct datatype *dtype, bool is_forward_declaration)
