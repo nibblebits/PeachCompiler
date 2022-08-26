d7c57b5a10d59e4cd25858760d01b7763fc8fc36
Daniel McCarthy <daniel@dragonzap.com>
2022-08-26 11:24:07 +0100

    Lecture 156 - Generating statement return

--- codegen.c
+++ codegen.c
@@ -251,6 +251,18 @@ void asm_pop_ebp()
     asm_push_ins_pop("ebp", STACK_FRAME_ELEMENT_TYPE_SAVED_BP, "function_entry_saved_ebp");
 }
 
+void codegen_stack_add_no_compile_time_stack_frame_restore(size_t stack_size)
+{
+    if (stack_size != 0)
+    {
+        asm_push("add esp, %lld", stack_size);
+    }
+}
+void asm_pop_ebp_no_stack_frame_restore()
+{
+    asm_push("pop ebp");
+}
+
 void codegen_stack_sub_with_name(size_t stack_size, const char *name)
 {
     if (stack_size != 0)
@@ -1653,6 +1665,36 @@ void codegen_generate_structure_push(struct resolver_entity* entity, struct hist
     asm_push("; END STRUCTURE PUSH");
     codegen_response_acknowledge(RESPONSE_SET(.flags=RESPONSE_FLAG_PUSHED_STRUCTURE));
 }
+
+
+void codegen_generate_statement_return_exp(struct node* node)
+{
+    codegen_response_expect();
+    codegen_generate_expressionable(node->stmt.return_stmt.exp, history_begin(IS_STATEMENT_RETURN));
+    struct datatype dtype;
+    assert(asm_datatype_back(&dtype));
+    if (datatype_is_struct_or_union_non_pointer(&dtype))
+    {
+        asm_push("mov edx, [ebp+8]");
+        codegen_generate_move_struct(&dtype, "edx", 0);
+        asm_push("mov eax, [ebp+8]");
+        return;
+    }
+
+    asm_push_ins_pop("eax", STACK_FRAME_ELEMENT_TYPE_PUSHED_VALUE, "result_value");
+}
+
+void codegen_generate_statement_return(struct node* node)
+{
+    if (node->stmt.return_stmt.exp)
+    {
+        codegen_generate_statement_return_exp(node);
+    }
+
+    codegen_stack_add_no_compile_time_stack_frame_restore(C_ALIGN(function_node_stack_size(node->binded.function)));
+    asm_pop_ebp_no_stack_frame_restore();
+    asm_push("ret");
+}
 void codegen_generate_statement(struct node *node, struct history *history)
 {
     switch (node->type)
@@ -1661,9 +1703,18 @@ void codegen_generate_statement(struct node *node, struct history *history)
         codegen_generate_exp_node(node, history_begin(history->flags));
         break;
 
+    case NODE_TYPE_UNARY:
+        codegen_generate_unary(node, history_begin(history->flags));
+        break;
+
     case NODE_TYPE_VARIABLE:
         codegen_generate_scope_variable(node);
         break;
+
+    case NODE_TYPE_STATEMENT_RETURN:
+        codegen_generate_statement_return(node);
+        break;
+
     }
 
     codegen_discard_unused_stack();
--- test.c
+++ test.c
@@ -6,14 +6,16 @@ struct dog
     char kk;
 };
 
+struct dog get_dog()
+{
+    struct dog d;
+    d.b = 50;
+    return d;
+}
 
+struct dog a;
 int main()
 {
-    int* p;
-    int b;
-    b = 50;
-    p = &b;
-    int e;
-    e = *p;
-
+    a = get_dog();
+    return a.b;
 }
