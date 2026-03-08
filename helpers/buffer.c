81d61c6cf0662a9462e21945445eb2d510aa27c1
Daniel McCarthy <dansblackberry2@hotmail.co.uk>
2026-03-08 13:09:35 +0200

    Added licensing in the files

--- helpers/buffer.c
+++ helpers/buffer.c
@@ -1,3 +1,27 @@
+/*
+ * PeachCompiler C Compiler Project
+ * Copyright (C) 2026 Daniel McCarthy <daniel@dragonzap.com>
+ * This file is part of the PeachCompiler.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License version 2 for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <https://www.gnu.org/licenses/>.
+ *
+ * For full source code, documentation, and structured learning,
+ * see the official compiler development video course by Daniel McCarthy here:
+ * dragonzap.com/course/creating-a-c-compiler-from-scratch
+ *
+ * Learn to build your own C compiler from scratch with that video course over 39 hours of video content available.
+ */
+
 #include "buffer.h"
 #include <stdlib.h>
 #include <stdio.h>
